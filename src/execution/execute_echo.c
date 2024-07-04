e0f3324481fcefa50122855fdfa4e289572c38b7
Oleh Ruban <oruban@c1r10s2.42wolfsburg.de>
2024-07-04 13:24:43 +0200

    case 'echo -n -n JKLJ -n' alined with bash - execute_echo()

--- src/execution/execute_echo.c
+++ src/execution/execute_echo.c
@@ -6,7 +6,7 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:18:46 by oruban            #+#    #+#             */
-/*   Updated: 2024/07/01 19:31:58 by oruban           ###   ########.fr       */
+/*   Updated: 2024/07/04 17:21:56 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,12 @@ int	echo_handle_option(char **args)
 }
 
 /* 
-	roi 0612
+	roi 0612-0704
 	if there is not opion '-n' -> no_newline = 0 and 
 	 '\n' after the last argument is being printed
 	all arguments are printed out with a space between them
+	0704 - muliple -n flags before argument are ignored like in bash,
+	cases like 'echo -n -n JKL -n'
  */
 int	execute_echo(char *args[], int fd_out)
 {
@@ -48,13 +50,16 @@ int	execute_echo(char *args[], int fd_out)
 	int		i;
 	int		first_arg_printed;
 
-	i = 1;
 	first_arg_printed = 0;
 	no_newline = echo_handle_option(args);
-	if (no_newline)
-		i++;
+	i = 1 + no_newline - 1;
 	while (args[i])
 	{
+		if (!strcmp (args[i], "-n") && !first_arg_printed)
+		{
+			i++;
+			continue ;
+		}
 		if (args[i][0] != '\0')
 		{
 			if (first_arg_printed)
@@ -68,3 +73,36 @@ int	execute_echo(char *args[], int fd_out)
 		ft_putstr_fd("\n", fd_out);
 	return (0);
 }
+
+/* int	execute_echo(char *args[], int fd_out)
+{
+	int		no_newline;
+	int		i;
+	int		first_arg_printed;
+
+	i = 1;
+	first_arg_printed = 0;
+	no_newline = echo_handle_option(args);
+	if (no_newline)
+		i++;
+	while (args[i])
+	{
+		if (!strcmp (args[i], "-n") && !first_arg_printed)
+		{
+			i++;
+			continue ;
+		}
+		if (args[i][0] != '\0')
+		{
+			if (first_arg_printed)
+				ft_putstr_fd(" ", fd_out);
+			ft_putstr_fd(args[i], fd_out);
+			first_arg_printed = 1;
+		}
+		i++;
+	}
+	if (!no_newline)
+		ft_putstr_fd("\n", fd_out);
+	return (0);
+}
+ */
